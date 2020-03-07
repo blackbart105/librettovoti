59e56029cfc4bd034b4829ab6d9f700b68e4d42a
vasci <vasci@LAPTOP-I2CLVJR5>
2020-03-07 14:32:21 +0100

    classe libretto vuota

--- src/salso/miracle/libretto/voto.java
+++ src/salso/miracle/libretto/voto.java
@@ -3,5 +3,29 @@ import java.time.LocalDate;
 public class voto {
 		private int voto;
 		private String corso;
-		private Localdate data;
+		public voto(int voto, String corso, LocalDate data) {
+			super();
+			this.voto = voto;
+			this.corso = corso;
+			this.data = data;
+		}
+		private LocalDate data;
+		public int getVoto() {
+			return voto;
+		}
+		public void setVoto(int voto) {
+			this.voto = voto;
+		}
+		public String getCorso() {
+			return corso;
+		}
+		public void setCorso(String corso) {
+			this.corso = corso;
+		}
+		public LocalDate getData() {
+			return data;
+		}
+		public void setData(LocalDate data) {
+			this.data = data;
+		}
 }
